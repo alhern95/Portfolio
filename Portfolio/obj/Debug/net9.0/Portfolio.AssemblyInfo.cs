0da45441e2aded281a039845efd7af03da41be22
alhern95 <hernandezalvaro80@mgail.com>
2025-06-11 12:41:48 -0700

    added images and cards for experience

--- Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab637b4c806c101bfc73445967a759a937c1dbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7013e274b5ce6de854c4142d6debc8ffc09c57b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
